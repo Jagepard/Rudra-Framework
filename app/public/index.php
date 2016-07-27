b4885c8e4c65f663205f839556d5aab67cf87e99
Jagepard <dankorot@gmail.com>
2016-07-27 12:10:35 +0300

    Updated namespaces

--- app/public/index.php
+++ app/public/index.php
@@ -23,13 +23,13 @@ require BP . 'vendor/autoload.php';
 
 $di = new \Rudra\Container();
 
-$di->set('dbClass', new \Rudra\DB($di, \Config\Config::DB));
+$di->set('dbClass', new \Rudra\DB($di, App\Config\Config::DB));
 
-$di->set('redirect', new \Core\Redirect(\Config\Config::URI));
+$di->set('redirect', new \Rudra\Redirect(App\Config\Config::URI));
 
-$di->set('validation', new \Rudra\Validation(\Config\Config::CAPTHA_SECRET));
+$di->set('validation', new \Rudra\Validation(App\Config\Config::CAPTHA_SECRET));
 
-$app = new \Config\Routing(
+$app = new App\Config\Routing(
     new \Rudra\Router($di)
 );
 
