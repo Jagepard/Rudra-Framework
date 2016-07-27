93a45063f16064e28fb60e2714a2e812bbd70b0f
Jagepard <dankorot@gmail.com>
2016-07-27 14:58:45 +0300

    use App\Main\Module;

--- app/main/controller/MainController.php
+++ app/main/controller/MainController.php
@@ -1,13 +1,13 @@
 <?php namespace App\Main\Controller;
 
 
-use App\Main;
+use App\Main\Module;
 
 /**
  * Class MainController
  * @package Main
  */
-class MainController extends Main
+class MainController extends Module
 {
     public function actionIndex()
     {
