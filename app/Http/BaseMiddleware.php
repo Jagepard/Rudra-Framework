c9c87b8732135854c3a577d2e176cd7143ad81d8
Jagepard <dankorot@gmail.com>
2017-04-18 17:53:21 +0300

    Добавляет BaseMiddleware

---
+++ app/Http/BaseMiddleware.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * Date: 17.04.17
+ * Time: 14:19
+ *
+ * @author    : Korotkov Danila <dankorot@gmail.com>
+ * @copyright Copyright (c) 2016, Korotkov Danila
+ * @license   http://www.gnu.org/licenses/gpl.html GNU GPLv3.0
+ */
+
+namespace App\Http;
+
+
+use Rudra\ContainerInterface;
+
+
+/**
+ * Class BaseMiddleware
+ *
+ * @package stub
+ */
+abstract class BaseMiddleware
+{
+
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    /**
+     * Middleware constructor.
+     *
+     * @param ContainerInterface $container
+     */
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * @param null $middleware
+     *
+     * @return bool
+     */
+    abstract public function __invoke($middleware = null);
+
+    /**
+     * @return ContainerInterface
+     */
+    public function container(): ContainerInterface
+    {
+        return $this->container;
+    }
+
+    /**
+     * @param $middleware
+     */
+    protected function next($middleware)
+    {
+        if (isset($middleware[1])) {
+            (new $middleware[1][0]($this->container()))(array_pop($middleware));
+        }
+    }
+
+    /**
+     * @param $middleware
+     *
+     * @return mixed
+     */
+    protected function handleArray($middleware)
+    {
+        if (!is_array($middleware[0])) {
+            $middleware[0] = $middleware;
+            unset($middleware[1]);
+
+            return $middleware;
+        }
+
+        return $middleware;
+    }
+}
