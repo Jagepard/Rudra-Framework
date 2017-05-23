32753d34750a472bc0ec276f3e3bb3e3029acf34
Jagepard <dankorot@gmail.com>
2017-05-23 17:13:17 +0300

    Добавляет return $this->twig

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,13 +6,12 @@ use App\Http\HttpController;
 
 class MainController extends HttpController
 {
+
     /**
      * @Routing(url = '')
      */
     public function actionIndex()
     {
-        $this->setData('content', $this->view('index', $this->getData()));
-
-        return $this->render('layout', $this->getData());
+        return $this->twig('index.html.twig', $this->data());
     }
 }
