a5c005d3f4e64024b770271b23bfd241055216f2
Jagepard <dankorot@gmail.com>
2017-05-23 15:11:28 +0300

    Удаляет комментарий

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -1,19 +1,8 @@
 <?php
 
-/**
- * Date: 20.03.17
- * Time: 15:46
- *
- * @author    : Korotkov Danila <dankorot@gmail.com>
- * @copyright Copyright (c) 2016, Korotkov Danila
- * @license   http://www.gnu.org/licenses/gpl.html GNU GPLv3.0
- */
-
 namespace App\Http\Controllers;
 
-
 use App\Http\BaseController;
-
 
 class MainController extends BaseController
 {
