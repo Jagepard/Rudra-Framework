be1030fbb618f8adc73c2af3d0c3f951d5e285b6
Jagepard <dankorot@gmail.com>
2017-05-24 11:42:23 +0300

    Удаляет лишнее

--- app/Http/HttpMiddleware.php
+++ app/Http/HttpMiddleware.php
@@ -9,29 +9,8 @@ class HttpMiddleware
 
     use SetContainerTrait;
 
-    /**
-     * @param null $middleware
-     *
-     * @return bool
-     */
-    public function __invoke($middleware = null)
-    {
-        // StartMiddleware
-
-        if ($middleware[0][1]['int'] % 2) {
-            echo json_encode($_SERVER);
-        }
-
-        // EndMiddleware
-
-        $this->next($middleware);
-    }
-
-    /**
-     * @param $middleware
-     */
     protected function next($middleware)
     {
-        $this->container()->get('router')->handleMiddleware($middleware, 1);
+        $this->container()->get('router')->handleMiddleware($middleware);
     }
 }
