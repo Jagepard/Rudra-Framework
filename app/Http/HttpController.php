e19e32557121880d24ae42ab7f2cd2f001537d5f
Jagepard <dankorot@gmail.com>
2017-09-07 19:56:27 +0300

    Выносит функционал в трейты

--- app/Http/HttpController.php
+++ app/Http/HttpController.php
@@ -2,7 +2,8 @@
 
 namespace App\Http;
 
-use Rudra\Container;
+use App\Http\Supports\HttpErrors;
+use App\Http\Supports\TwigFunctions;
 use Rudra\ContainerInterface;
 use Rudra\Controller;
 
@@ -14,6 +15,9 @@ use Rudra\Controller;
 class HttpController extends Controller
 {
 
+    use TwigFunctions;
+    use HttpErrors;
+
     /**
      * @param ContainerInterface $container
      * @param string             $templateEngine
@@ -21,90 +25,9 @@ class HttpController extends Controller
     public function init(ContainerInterface $container, string $templateEngine)
     {
         parent::init($container, $templateEngine);
-        $this->setData('Rudra', 'title');
-    }
 
-    /**
-     * Метод выполняется перед вызовом контроллера
-     */
-    public function before()
-    {
-        $this->container()->get('auth')->check();
-    }
-
-    public function after()
-    {
-        // Очищаем сессию от alert
-        $this->container()->unsetSession('value');
-        $this->container()->unsetSession('alert');
-    }
-
-    public function templateEngine(string $config): void
-    {
-        parent::templateEngine($config);
-
-        $d = new \Twig_SimpleFunction('d', function ($var) {
-            return d($var);
-        });
-
-        $this->getTwig()->addFunction($d);
-
-        $date = new \Twig_SimpleFunction('date', function ($var) {
-            return date($var);
-        });
-
-        $this->getTwig()->addFunction($date);
-
-        $auth = new \Twig_SimpleFunction('auth', function () {
-            return $this->container()->get('auth')->regularAccess();
-        });
-
-        $this->getTwig()->addFunction($auth);
-
-
-        $active = new \Twig_SimpleFunction('active', function ($var) {
-            return $this->container()->get('pagination')->activeClass($var);
-        });
-
-        $this->getTwig()->addFunction($active);
-
-        $strstr = new \Twig_SimpleFunction('strstr', function ($var) {
-            return strstr($var, '<a id="strstr" name="strstr"></a>', true);
-        });
-
-        $this->getTwig()->addFunction($strstr);
-
-        if (DEV) {
-            $debugbarRenderer = Container::$app->get('debugbar')->getJavascriptRenderer();
-            $this->getTwig()->addGlobal('debugbar', $debugbarRenderer);
-        }
-
-    }
-
-    public function error404()
-    {
-        $this->container()->get('redirect')->responseCode('404');
-
-        return $this->twig('errors/error.html.twig', [
-            'title'  => '404 Page Not Found :: ' . $this->data('title'),
-        ]);
-    }
-
-    public function error503()
-    {
-        $this->container()->get('redirect')->responseCode('503');
-
-        return $this->twig('errors/error.html.twig', [
-            'title'  => '503 Service Unavailable :: ' . $this->data('title'),
-        ]);
-    }
-
-    public function error500()
-    {
-        $this->container()->get('redirect')->responseCode('503');
-
-        return $this->twig('errors/503.html.twig', [
-            'title'  => '503 Service Unavailable :: ' . $this->data('title'),
-        ]);
+        $this->setData('TDcoRe || *Transition metal', 'title');
+        $this->setData($this->container(), 'container');
+        $this->check();
     }
 }
