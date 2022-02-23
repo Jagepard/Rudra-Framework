b9ad3c936132765ed8e030009ff26764c0a18150
Jagepard <Jagepard@yandex.ru>
2022-02-23 12:12:41 +0300

    DATETIME to TIMESTAMP

--- app/Ship/Commands/CreateMigrationCommand.php
+++ app/Ship/Commands/CreateMigrationCommand.php
@@ -89,8 +89,8 @@ class {$className}_migration
         \$query = Rudra::get("DSN")->prepare("
             CREATE TABLE {\$table} (
             `id` INT NOT NULL AUTO_INCREMENT ,
-            `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ,
-            `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+            `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+            `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
                 PRIMARY KEY (`id`)
             ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci
         ");
