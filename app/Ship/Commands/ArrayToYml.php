a9f182f5b267456be3a0a4b2692f2ea83eabd3ec
Jagepard <dankorot@gmail.com>
2022-08-24 17:39:10 +0300

    конвертер массивов в yaml

---
+++ app/Ship/Commands/ArrayToYml.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Ship\Commands;
+
+use Exception;
+use Symfony\Component\Yaml\Yaml;
+use Rudra\Cli\ConsoleFacade as Cli;
+
+class ArrayToYml
+{
+    public function actionIndex()
+    {
+        Cli::printer("Put the file containing the array into a directory\n", "green");
+        Cli::printer("Enter filename with php Array: ", "magneta");
+        $filename = trim(fgets(fopen("php://stdin", "r")));
+
+        try {
+            $array = include("config/$filename.php");
+            $yaml = Yaml::dump($array);
+            file_put_contents("config/$filename.yml", $yaml);
+
+            Cli::printer("Yml was created" . PHP_EOL, "cyan", );
+        } catch (Exception $e) {
+            echo 'Exception: ',  $e->getMessage(), "\n";
+        }
+    }
+}
