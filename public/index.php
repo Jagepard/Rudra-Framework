fd59f3bb38f436d853cf891df950521e65a4bab3
Jagepard <dankorot@gmail.com>
2023-02-16 18:47:39 +0300

    removed facades

--- public/index.php
+++ public/index.php
@@ -3,35 +3,34 @@
 require "../vendor/autoload.php";
 
 use App\Ship\Route;
+use Rudra\Auth\Auth;
+use Rudra\Container\Rudra;
 use Symfony\Component\Yaml\Yaml;
-use Rudra\Container\Facades\Rudra;
-use Rudra\Container\Facades\Request;
-use Rudra\Auth\AuthFacade as Auth;
 
 (new Whoops\Run)->appendHandler(new Whoops\Handler\PrettyPageHandler)->register();
 
-Rudra::config((php_sapi_name() == "cli-server")
+Rudra::run()->config((php_sapi_name() == "cli-server")
     ? Yaml::parseFile("../config/setting.local.yml")
     : Yaml::parseFile("../config/setting.production.yml"));
 
-Rudra::config()->set(["url" => (php_sapi_name() == "cli-server")
+Rudra::run()->config()->set(["url" => (php_sapi_name() == "cli-server")
     ? "http://127.0.0.1:8000"
-    : Request::server()->get("REQUEST_SCHEME") . "://" . Request::server()->get("SERVER_NAME")]);
+    : Rudra::run()->request()->server()->get("REQUEST_SCHEME") . "://" . Rudra::run()->request()->server()->get("SERVER_NAME")]);
 
-Rudra::config()->set(require_once "../app/Ship/Services.php");
+Rudra::run()->config()->set(require_once "../app/Ship/Services.php");
+Rudra::run()->binding(Rudra::run()->config()->get("contracts"));
+Rudra::run()->services(Rudra::run()->config()->get("services"));
 
-Rudra::binding(Rudra::config()->get("contracts"));
-Rudra::services(Rudra::config()->get("services"));
-
-if (Rudra::config()->get("environment") === "development") {
-    Rudra::get("debugbar")->addCollector(new DebugBar\DataCollector\PDO\PDOCollector(new DebugBar\DataCollector\PDO\TraceablePDO(Rudra::get("DSN"))));
-    Rudra::get("debugbar")->addCollector(new DebugBar\DataCollector\ConfigCollector(Rudra::config()->get()));
-    Rudra::get("debugbar")["time"]->startMeasure("application");
-    Rudra::get("debugbar")["time"]->startMeasure("index");
+if (Rudra::run()->config()->get("environment") === "development") {
+    Rudra::run()->get("debugbar")->addCollector(new DebugBar\DataCollector\PDO\PDOCollector(new DebugBar\DataCollector\PDO\TraceablePDO(Rudra::run()->get("DSN"))));
+    Rudra::run()->get("debugbar")->addCollector(new DebugBar\DataCollector\ConfigCollector(Rudra::run()->config()->get()));
+    Rudra::run()->get("debugbar")["time"]->startMeasure("application");
+    Rudra::run()->get("debugbar")["time"]->startMeasure("index");
 }
 
-session_name("RSID_" . Auth::getSessionHash());
-Rudra::get(Route::class)->run();
+session_name("RSID_" . Rudra::run()->get(Auth::class)->getSessionHash());
+
+Rudra::run()->get(Route::class)->run();
 
 /*
  | php rudra serve to run built-in web server
