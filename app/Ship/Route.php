f639f6ea823e5539bdf0fedf995d88d5afa69d15
Jagepard <jagepard@yandex.ru>
2025-05-21 12:37:51 +0300

    adds routing cache

--- app/Ship/Route.php
+++ app/Ship/Route.php
@@ -14,24 +14,62 @@ class Route
     public function run()
     {
         if (config('environment') === 'development') {
-            Rudra::get("debugbar")['time']->stopMeasure('index');
-            Rudra::get("debugbar")['time']->startMeasure('routing');
+            $timeCollector = Rudra::get("debugbar")['time'];
+
+            if ($timeCollector->hasStartedMeasure('index')) {
+                $timeCollector->stopMeasure('index');
+            }
+
+            if (!$timeCollector->hasStartedMeasure('routing')) {
+                $timeCollector->startMeasure('routing');
+            }
         }
 
         $this->collect(Rudra::config()->get('containers'));
-        throw new RouterException("404");
+
+        if (config('environment') !== 'test') {
+            exit();
+        }
     }
 
     protected function collect(array $namespaces): void
     {
         foreach ($namespaces as $container => $item) {
-            $this->getRoutes($container);
+            $routes = $this->getRoutes($container);
+
+            // Объединяем все маршруты в один массив
+            $flatRoutes = array_merge(...$routes);
+
+            // Регистрируем маршруты
+            foreach ($flatRoutes as $route) {
+                Router::set($route);
+            }
         }
     }
 
-    protected function getRoutes(string $container): ?array
+    protected function getRoutes(string $container): array
     {
+        $cacheDir = "../app/cache";
+        $cacheFile = $cacheDir . "/routes_" . $container . ".php";
+
+        if (!is_dir($cacheDir)) {
+            mkdir($cacheDir, 0755, true);
+        }
+
+        if (file_exists($cacheFile)) {
+            return include $cacheFile;
+        }
+
         $path = "../app/Containers/" . ucfirst($container) . "/routes";
-        return Router::annotationCollector(require_once $path . ".php", false, Rudra::config()->get("attributes"));
+
+        if (!file_exists($path . ".php")) {
+            throw new \RuntimeException("Routes file not found for container: " . $container);
+        }
+
+        $routes = Router::annotationCollector(require_once $path . ".php", true, Rudra::config()->get("attributes"));
+        file_put_contents($cacheFile, "<?php\nreturn " . var_export($routes, true) . ";");
+
+        return $routes;
     }
+    
 }
