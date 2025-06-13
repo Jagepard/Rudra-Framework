94e5f5d43df83961b146468804b96243afe715e2
Jagepard <jagepard@yandex.ru>
2025-06-13 15:54:31 +0300

    add Schema

--- app/Ship/Migration/Users_21012021130204_migration.php
+++ app/Ship/Migration/Users_21012021130204_migration.php
@@ -2,28 +2,23 @@
 
 namespace App\Ship\Migration;
 
-use Rudra\Model\QBFacade;
+use Rudra\Model\Schema;
 use Rudra\Container\Facades\Rudra;
 
 class Users_21012021130204_migration
 {
     public function up(): void
     {
-        $table = "users";
-
-        $qString = QBFacade::create($table)
-            ->integer('id', '', true)
-            ->string('name')
-            ->string('email', 'NOT NULL UNIQUE')
-            ->string('password')
-            ->string('role')
-            ->integer('status', 'DEFAULT 1')
-            ->created_at()
-            ->updated_at()
-            ->pk('id')
-            ->close()
-            ->get();
-
-        Rudra::get("DSN")->prepare("$qString")->execute();
+        Schema::create('users', function ($table) {
+            $table->integer('id', '', true)
+                ->string('name')
+                ->string('email', 'NOT NULL UNIQUE')
+                ->string('password')
+                ->string('role')
+                ->integer('status', 'DEFAULT 1')
+                ->created_at()
+                ->updated_at()
+                ->pk('id');
+        })->execute();
     }
 }
